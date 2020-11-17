541de473cbb9a29fd0531b732d2cb79f67300e8d
Vladimir Vozniak <vozniak.vladimir90@gmail.com>
2020-11-17 14:15:45 +0300

    Add some changes to classes Main and Library

--- Books/src/Library.java
+++ Books/src/Library.java
@@ -1,5 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Library {
     private static Book[] books;
+    private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public Library(Book... newBooks) {
         cloneArray(newBooks);
@@ -23,6 +28,54 @@ public class Library {
         for (; index2 < newBooks.length; index2++) {
             books[index] = newBooks[index2];
             index++;
+        }
+    }
+
+    public void chooseOption() throws IOException {
+        boolean stop = false;
+        int option = 0;
+        do{
+            System.out.println("Please choose one of the suggested options and enter its number.");
+            System.out.println("1. Display a list of books by the specified author.");
+            System.out.println("2. Display a list of books published by the specified publisher.");
+            System.out.println("3. Display a list of books released after the specified year.");
+            try {
+                option = Integer.parseInt(reader.readLine());
+            } catch (Exception e) {
+                System.out.println("Invalid data.");
+            }
+            if(option == 1 || option == 2 || option == 3) stop = true;
+            else System.out.println("Please try again");
+        } while (!stop);
+        chooseBooks(option);
+    }
+
+    private void chooseBooks(int option) throws IOException {
+        switch (option) {
+            case 1 -> {
+                System.out.println("Please enter author name:");
+                String author = reader.readLine();
+                for (int i = 0; i < this.getBooks().length; i++) {
+                    if(this.getBooks()[i].getAuthor().equalsIgnoreCase(author))
+                        System.out.println(this.getBooks()[i]);
+                }
+            }
+            case 2 -> {
+                System.out.println("Please enter publishing house:");
+                String publisher = reader.readLine();
+                for (int i = 0; i < this.getBooks().length; i++) {
+                    if(this.getBooks()[i].getPublishingHouse().equalsIgnoreCase(publisher))
+                        System.out.println(this.getBooks()[i]);
+                }
+            }
+            case 3 -> {
+                System.out.println("Please enter the year:");
+                int year = Integer.parseInt(reader.readLine());
+                for (int i = 0; i < this.getBooks().length; i++) {
+                    if(this.getBooks()[i].getPublicationYear() > year)
+                        System.out.println(this.getBooks()[i]);
+                }
+            }
         }
     }
 }
--- Books/src/Main.java
+++ Books/src/Main.java
@@ -16,54 +16,6 @@ public class Main {
         Book book5 = new Book("Inferno", "Brown",
                 "AST", 2016, 543, 25, "solid");
         Library library = new Library(book1, book2, book3, book4, book5);
-        chooseOption(library);
-    }
-
-    private static void chooseOption(Library library) throws IOException {
-        boolean stop = false;
-        int option = 0;
-        do{
-            System.out.println("Please choose one of the suggested options and enter its number.");
-            System.out.println("1. Display a list of books by the specified author.");
-            System.out.println("2. Display a list of books published by the specified publisher.");
-            System.out.println("3. Display a list of books released after the specified year.");
-            try {
-                option = Integer.parseInt(reader.readLine());
-            } catch (Exception e) {
-                System.out.println("Invalid data.");
-            }
-            if(option == 1 || option == 2 || option == 3) stop = true;
-            else System.out.println("Please try again");
-        } while (!stop);
-        getBooks(option, library);
-    }
-
-    private static void getBooks(int option, Library library) throws IOException {
-        switch (option) {
-            case 1 -> {
-                System.out.println("Please enter author name:");
-                String author = reader.readLine();
-                for (int i = 0; i < library.getBooks().length; i++) {
-                    if(library.getBooks()[i].getAuthor().equalsIgnoreCase(author))
-                        System.out.println(library.getBooks()[i]);
-                }
-            }
-            case 2 -> {
-                System.out.println("Please enter publishing house:");
-                String publisher = reader.readLine();
-                for (int i = 0; i < library.getBooks().length; i++) {
-                    if(library.getBooks()[i].getPublishingHouse().equalsIgnoreCase(publisher))
-                        System.out.println(library.getBooks()[i]);
-                }
-            }
-            case 3 -> {
-                System.out.println("Please enter the year:");
-                int year = Integer.parseInt(reader.readLine());
-                for (int i = 0; i < library.getBooks().length; i++) {
-                    if(library.getBooks()[i].getPublicationYear() > year)
-                        System.out.println(library.getBooks()[i]);
-                }
-            }
-        }
+        library.chooseOption();
     }
 }
